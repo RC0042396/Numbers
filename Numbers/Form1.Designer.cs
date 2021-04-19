d3c7b9c9f340bb025dc6b9c17e673a877760975d
Jaymie Lucas <RC0042396@student.riversidecollege.ac.uk>
2021-04-19 15:21:40 +0100

    Add project files.

---
+++ Numbers/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Numbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // RichTextBox
+            // 
+            this.RichTextBox.Location = new System.Drawing.Point(325, 147);
+            this.RichTextBox.Name = "RichTextBox";
+            this.RichTextBox.Size = new System.Drawing.Size(100, 96);
+            this.RichTextBox.TabIndex = 1;
+            this.RichTextBox.Text = "";
+            this.RichTextBox.TextChanged += new System.EventHandler(this.RichTextBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RichTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox RichTextBox;
+    }
+}
+
---
+++ Numbers/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            double d1 = 5e6;
+            RichTextBox.AppendText(d1.ToString() + "\n");
+            RichTextBox.AppendText(d1.ToString("N") + "\n");
+            RichTextBox.AppendText(d1.ToString("N3") + "\n");
+            RichTextBox.AppendText(d1.ToString("N0") + "\n");
+            RichTextBox.AppendText(d1.ToString("0") + "\n");
+            RichTextBox.AppendText(d1.ToString("0.0") + "\n");
+            RichTextBox.AppendText(d1.ToString("0.0E0") + "\n");
+            RichTextBox.AppendText(d1.ToString("0.00E00") + "\n");
+
+            double pi = 3.141592653589793;
+            double c = 299792458;
+
+            RichTextBox.AppendText(pi.ToString("N3") + "\n");
+            RichTextBox.AppendText(pi.ToString("N5") + "\n");
+            RichTextBox.AppendText(pi.ToString("N15") + "\n");
+
+            RichTextBox.AppendText(c.ToString() + "\n");
+            RichTextBox.AppendText(c.ToString("N0") + "\n");
+            RichTextBox.AppendText(c.ToString("0.000000000000000E0") + "\n");
+            RichTextBox.AppendText(c.ToString("0.000E0") + "\n");
+
+
+
+
+        }
+
+        private void RichTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
